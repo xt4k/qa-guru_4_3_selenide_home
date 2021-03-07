d63536b95a9f1028641cebacfaa11700b09b9dd9
Yuriy Logvinov <yuriy.logvinov@adonis.no>
2021-03-07 21:02:05 +0200

    added class

---
+++ src/test/java/guru/qa/selenide/github/FindSelenideSoftAssertionPageTest.java
@@ -0,0 +1,26 @@
+package guru.qa.selenide.github;
+
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FindSelenideSoftAssertionPageTest {
+
+  @Test
+  void shouldFindSelenideInGitHub(){
+    // открыть страничку гитхаб
+    open("https://github.com");
+    // ввести в поле поиска selenide
+    // нажать enter
+    $("[name=q]").setValue("selenide").pressEnter();
+    // выбрать первый найденый проект из списка
+    //$$("ul.repo-list li").first().$("a").click();
+    $("ul.repo-list li a").click();
+    // проверка: в заголовке встречается selenide/selenide
+    $("h1").shouldHave(text("selenide / selenide"));
+  }
+
+}
